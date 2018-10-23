3e7d857aaebd9f7042e2526f46d295e35f131fea
Israel Andrade <israelg.andrade@bol.com.br>
2018-10-22 23:30:54 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/iga/cursomc/services/CategoriaService.java
+++ src/main/java/com/iga/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.iga.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.iga.cursomc.domain.Categoria;
 import com.iga.cursomc.repositories.CategoriaRepository;
+import com.iga.cursomc.services.exceptions.DataIntegrityException;
 import com.iga.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -33,6 +35,17 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId()); // o find irá buscar a categoria pelo ID
 		return repo.save(obj);
+	}
+
+	// Método para deletar pelo id
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produto");
+		}
+		
 	}
 	
 	
