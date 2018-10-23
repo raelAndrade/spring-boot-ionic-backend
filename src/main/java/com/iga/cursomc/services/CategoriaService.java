f91a785c34056fc30784863d012454110f2285be
Israel Andrade <israelg.andrade@bol.com.br>
2018-10-22 23:49:05 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/iga/cursomc/services/CategoriaService.java
+++ src/main/java/com/iga/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.iga.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -46,6 +48,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produto");
 		}
 		
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 	
 	
