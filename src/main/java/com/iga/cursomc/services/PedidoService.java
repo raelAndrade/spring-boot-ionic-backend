70af3a1630610b40ce7507690c7e5ee8de409d07
Israel Andrade <israelg.andrade@bol.com.br>
2021-05-13 00:38:49 -0300

    Refactor methods spring boot 2.x

--- src/main/java/com/iga/cursomc/services/PedidoService.java
+++ src/main/java/com/iga/cursomc/services/PedidoService.java
@@ -1,11 +1,15 @@
 package com.iga.cursomc.services;
 
+import com.iga.cursomc.domain.Categoria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.iga.cursomc.domain.Pedido;
 import com.iga.cursomc.repositories.PedidoRepository;
 import com.iga.cursomc.services.exceptions.ObjectNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PedidoService {
@@ -14,14 +18,17 @@ public class PedidoService {
 	private PedidoRepository repo;
 	
 	// Método para buscar a categoria pelo o id
-	public Pedido find(Integer id) {
-		Pedido obj = repo.findOne(id);
+	public Optional<Pedido> find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado!: Id: " + id
 					+ ", Tipo: " + Pedido.class.getName());
 		}
 		return obj;
 	}
-	
+
+	public List<Pedido> findAll() {
+		return repo.findAll();
+	}
 	
 }
